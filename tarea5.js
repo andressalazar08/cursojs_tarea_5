603f7635e9f367823cc57a607effc7b1dc6cd787
AndresSalazar <andres.f.salazar@correounivalle.edu.co>
2022-12-12 10:50:11 -0500

    tarea 5

---
+++ tarea5.js
@@ -0,0 +1,24 @@
+/*Crea un archivo JS que contenga las siguientes líneas
+
+- Una variable que contenga tu altura en centímetros (entero)
+
+- Una variable que contenga tu altura en metros (número de coma flotante)
+
+- Una variable que contenga tu peso en kilogramos (número de coma flotante)
+
+- Una variable que contenga tu altura en metros redondeada hacia arriba
+
+- Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+
+- Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript*/
+
+
+let altura=168;
+let altura2=1.68;
+let peso1=75.8;
+let altura_redondeada=Math.round(altura2,0)
+let altura_redondeada2=Math.floor(peso1)
+
+
+console.log(altura_redondeada);
+console.log(altura_redondeada2);
